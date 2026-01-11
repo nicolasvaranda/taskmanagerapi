03f1da6ea838518aeadf0157213cf99d82204b98
nicolasvaranda <nicolasbro@hotmail.com>
2026-01-10 22:35:02 -0300

    feat: working on entitys and h2

--- src/main/java/com/taskmanager/model/entity/Task.java
+++ src/main/java/com/taskmanager/model/entity/Task.java
@@ -1,4 +1,42 @@
 package com.taskmanager.model.entity;
 
+import com.taskmanager.model.enums.TaskStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks", indexes = {
+        @Index(name = "idx_user_id", columnList = "user_id"),
+        @Index(name = "idx_status", columnList = "status")
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Size(min = 3, max = 200)
+    private String title;
+    @Size(max = 1000)
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
 }
--- src/main/java/com/taskmanager/model/entity/User.java
+++ src/main/java/com/taskmanager/model/entity/User.java
@@ -1,11 +1,17 @@
 package com.taskmanager.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -19,5 +25,13 @@ public class User {
     @NotBlank
     @Size(min = 3, max = 100)
     private String name;
+    @Email
+    @Column(unique = true)
+    private String email;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
 
 }
